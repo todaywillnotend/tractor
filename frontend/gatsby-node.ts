5cb85cd132e4102233fcec84631bbce8e2120a98
Eloy <todaywillnotend@gmail.com>
2024-06-22 23:30:55 +0300

    Изменил конфиги

--- frontend/gatsby-node.ts
+++ frontend/gatsby-node.ts
@@ -13,12 +13,50 @@ const formatCatalogData = (
   }));
 };
 
+async function checkBackendAvailability(
+  url: string,
+  delay: number,
+  maxAttempts: number
+): Promise<boolean> {
+  for (let i = 0; i < maxAttempts; i++) {
+    try {
+      const response = await fetch(url, { method: "HEAD" });
+
+      if (response.ok) {
+        // Backend is available
+        console.log("Backend is up - executing command");
+        return true;
+      }
+      console.log("Backend is unavailable - sleeping");
+    } catch (error) {
+      // Ignoring the error as the fetch will fail if the backend is not available
+    }
+
+    // Waiting for the specified delay
+    await new Promise((resolve) => setTimeout(resolve, delay));
+  }
+
+  // Backend is not available
+  return false;
+}
+
 const createPages: GatsbyNode["createPages"] = async ({ actions, graphql }) => {
   const { createPage } = actions;
 
+  const isBackendAvailable = await checkBackendAvailability(
+    process.env.STRAPI_URL as string,
+    5000,
+    120
+  );
+
+  if (!isBackendAvailable) {
+    console.error("Backend is not available.");
+    return;
+  }
+
   try {
     const responseCatalog = await fetch(
-      `${process.env.GATSBY_BACKEND_URL}/api/catalogs?populate=*&pagination[pageSize]=8` as string,
+      `${process.env.STRAPI_URL}/api/catalogs` as string,
       {
         method: "GET",
         headers: {
@@ -26,6 +64,7 @@ const createPages: GatsbyNode["createPages"] = async ({ actions, graphql }) => {
         },
       }
     );
+
     const responseCatalogJson: TResponseCollection<TItemResponse> =
       await responseCatalog.json();
 
--- frontend/scripts/server.js
+++ frontend/scripts/server.js
@@ -3,4 +3,6 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
